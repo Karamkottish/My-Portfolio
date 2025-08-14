370c4388f955b444b8e3a05a3d82aa867ba01780
Karam Kottish <karamkottish@gmail.com>
2025-08-14 19:14:40 +0300

    fixed

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3892896163"
+    serviceWorkerVersion: "2719299160"
   }
 });
